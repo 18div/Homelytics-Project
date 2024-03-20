18724e433c5e0f9ad8fd1d4eb9d66010f9627167
18div <divyamishra63618@gmail.com>
2024-03-20 20:16:45 +0530

    Edit Accomomdation added 6th

--- clientside/src/pages/EditAccommodationpage.js
+++ clientside/src/pages/EditAccommodationpage.js
@@ -28,7 +28,7 @@ function EditAccommodationPage() {
     try {
       const response = await axios.put(`https://homelytics-project-server.onrender.com/places/update/${id}`, updatedData);
       if (response.data.success) {
-        alert("Changes doesnt Saved TRY AGAIN..")
+        //Comment
       } else {
         console.error("Failed to update accommodation:", response.data.error);
       }
